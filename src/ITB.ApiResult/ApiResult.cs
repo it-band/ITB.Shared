b66afd4ec3aec7147eab85bc9633f59ece2c379c
Denis Novogrodsky <denis.novogrodsky@ids-group.co.uk>
2020-05-19 15:19:33 +0300

    fix api result action executor

--- src/ITB.ApiResult/ApiResult.cs
+++ src/ITB.ApiResult/ApiResult.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using ITB.ResultModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Microsoft.Net.Http.Headers;
 
 namespace ITB.ApiResultModel
 {
@@ -39,44 +35,14 @@ namespace ITB.ApiResultModel
 
     public class ApiResultExecutor : IActionResultExecutor<ApiResult>
     {
-        private static readonly string DefaultContentType = new MediaTypeHeaderValue("application/json")
-        {
-            Encoding = Encoding.UTF8
-        }.ToString();
-
-        private readonly JsonOptions _options;
-        public ApiResultExecutor(IOptions<JsonOptions> options)
-        {
-            _options = options.Value;
-        }
-
         public virtual async Task ExecuteAsync(ActionContext context, ApiResult result)
         {
-            if (context == null)
+            var jsonResult = new JsonResult(result.Value)
             {
-                throw new ArgumentNullException(nameof(context));
-            }
+                StatusCode = result.StatusCode
+            };
 
-            if (result == null)
-            {
-                throw new ArgumentNullException(nameof(result));
-            }
-
-            var jsonSerializerOptions = _options.JsonSerializerOptions;
-
-            var response = context.HttpContext.Response;
-
-            response.ContentType = DefaultContentType;
-
-            if (result.StatusCode != null)
-            {
-                response.StatusCode = result.StatusCode.Value;
-            }
-
-            // Keep this code in sync with SystemTextJsonOutputFormatter
-            var responseStream = response.Body;
-            await JsonSerializer.SerializeAsync<object>(responseStream, result.Value, jsonSerializerOptions);
-            await responseStream.FlushAsync();
+            await jsonResult.ExecuteResultAsync(context);
         }
     }
 }
