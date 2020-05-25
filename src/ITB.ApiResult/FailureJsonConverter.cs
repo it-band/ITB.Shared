0d762d88849f3dad3cfd915942874d926aebbd5f
Denis Novogrodsky <denis.novogrodsky@ids-group.co.uk>
2020-05-25 18:38:39 +0300

    fix FailureJsonConverter

--- src/ITB.ApiResult/FailureJsonConverter.cs
+++ src/ITB.ApiResult/FailureJsonConverter.cs
@@ -8,6 +8,8 @@ namespace ITB.ApiResultModel
     // Example of how to implement DerivedTypeJsonConverter
     public class FailureJsonConverter : JsonConverter<Failure>
     {
+        protected readonly JsonEncodedText Message = JsonEncodedText.Encode("message");
+
         public override Failure Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             throw new NotImplementedException();
@@ -15,7 +17,20 @@ namespace ITB.ApiResultModel
 
         public override void Write(Utf8JsonWriter writer, Failure value, JsonSerializerOptions options)
         {
-            JsonSerializer.Serialize(writer, value, value.GetType(), options);
+            if (value.GetType() == typeof(Failure))
+            {
+                writer.WriteStartObject();
+
+                writer.WritePropertyName(Message);
+
+                writer.WriteStringValue(value.Message);
+
+                writer.WriteEndObject();
+            }
+            else
+            {
+                JsonSerializer.Serialize(writer, value, value.GetType(), options);
+            }
         }
     }
 }
